7e7ffc8adfa53ae356d891294a60a339a3e1d96b
Melanie-J-Baker <baker_mel@hotmail.com>
2023-03-17 17:38:52 +0000

    Finish styling and create function to show/hide drawer menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./style.css";
+
+const drawerMenu = (() => {
+  const openBtn = document.querySelector(".open-btn");
+  const menu = document.querySelector(".menu");
+  openBtn.addEventListener("click", function () {
+    openBtn.style.display = "none";
+    menu.style.display = "grid";
+  });
+
+  const exitBtn = document.querySelector(".exit-btn");
+  exitBtn.addEventListener("click", function () {
+    menu.style.display = "none";
+    openBtn.style.display = "block";
+  });
+})();
+
+export { drawerMenu };
