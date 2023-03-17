9a7905ec9d4b714340988fff6840cf232f51570b
Melanie-J-Baker <baker_mel@hotmail.com>
2023-03-17 12:16:58 +0000

    Create files and add webpack, loaders, npm scripts, ESLint and Prettier

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+module.exports = {
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      { test: /\.txt$/, use: "raw-loader" },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
